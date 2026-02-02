ef1225c90a2646b8e1290798521e111ec4657037
mateja503 <matejanikolikj503@gmail.com>
2026-02-02 21:39:59 +0100

    added footer and header

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Layout } from '../layout/layout';
+import { Footer } from '../footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Layout],
+  imports: [RouterOutlet, Layout, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
