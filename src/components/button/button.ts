475ee4ccf05620b89ed4900603b2b88288a40f0b
mateja503 <matejanikolikj503@gmail.com>
2026-03-19 22:03:09 +0100

    created button component

--- src/components/button/button.ts
+++ src/components/button/button.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './button.scss',
 })
 export class Button {
-
+  @Input() label: string = 'Click Me';
+  @Input() disabled: boolean = false;
 }
