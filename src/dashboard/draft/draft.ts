5c65fa032fe43e73d073c67a5ac06f9c2509e6a7
mateja503 <matejanikolikj503@gmail.com>
2026-03-20 15:47:34 +0100

    put a gird in the draft html

--- src/dashboard/draft/draft.ts
+++ src/dashboard/draft/draft.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import {MatGridListModule} from '@angular/material/grid-list';
+ 
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 
 @Component({
   selector: 'app-draft',
-  imports: [],
+  imports: [MatGridListModule],
   templateUrl: './draft.html',
   styleUrl: './draft.scss',
 })
-export class Draft {
 
+export class Draft {
+tiles: Tile[] = [
+  {text: 'Draft Which Legaue & Timer', cols: 12, rows: 1, color: '#fb04ff' },
+  {text: 'Draft teams which team is next to draft', cols: 4, rows: 9, color: '#222'},   // Use Hex for 'dark'
+  {text: 'Current Team that the user have drafterd', cols: 8, rows: 5, color: 'brown'}, 
+  {text: 'Trades happening and also make a trade on draft night', cols: 8, rows: 4, color: 'blue'},
+  // {text: 'Four', cols: 2, rows: 1, color: 'red'},
+];
 }
