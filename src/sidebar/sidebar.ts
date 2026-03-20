5382db1828bf5f63fed5b44bd3f900ee6cafeea0
mateja503 <matejanikolikj503@gmail.com>
2026-03-20 13:01:58 +0100

    styled the colord for the sidebar

--- src/sidebar/sidebar.ts
+++ src/sidebar/sidebar.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
-
+import { MatIconModule } from '@angular/material/icon'; // Import this
 @Component({
   selector: 'app-sidebar',
-  imports: [MatSidenavModule],
+  imports: [MatSidenavModule, MatIconModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
 })
