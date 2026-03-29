a271e55a5260b2e6e472019b000f341c2b249037
mateja503 <matejanikolikj503@gmail.com>
2026-03-29 15:55:39 +0200

    "list all teams for user" for team component

--- src/dashboard/team/team.ts
+++ src/dashboard/team/team.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { Button } from '../../components/button/button';
 
 @Component({
   selector: 'app-team',
-  imports: [],
+  imports: [MatInputModule, MatFormFieldModule, MatRadioModule, MatIconModule, Button, ReactiveFormsModule],
   templateUrl: './team.html',
   styleUrl: './team.scss',
 })
 export class Team {
 
+  form = new FormGroup({
+    teamName: new FormControl(''),
+  });
+  onSubmit() {
+    console.log('Submit', this.form.value.teamName);
+  }
 }
