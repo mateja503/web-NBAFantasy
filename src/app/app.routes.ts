90890e052acbfa1846284083ba85c66a3bd7e16c
mateja503 <matejanikolikj503@gmail.com>
2026-03-29 21:25:43 +0200

    created the join league component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { Trade } from '../dashboard/trade/trade';
 import { LeagueCreate } from '../dashboard/league/league';
 import { Home } from '../dashboard/home/home';
 import { Team } from '../dashboard/team/team'; 
+import { JoinLeague } from '../dashboard/join-league/join-league';
 export const routes: Routes = [
     { path: 'draft', component: Draft },
     { path: 'trade', component: Trade  },
     { path: 'league/create', component: LeagueCreate  },
     { path: 'home', component: Home },
+    { path: 'league/join', component: JoinLeague },
     { path: 'team', component: Team },
 ];
---
+++ src/dashboard/join-league/join-league.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-join-league',
+  imports: [],
+  templateUrl: './join-league.html',
+  styleUrl: './join-league.scss',
+})
+export class JoinLeague {
+
+}
