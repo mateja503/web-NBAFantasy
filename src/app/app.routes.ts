30db4e3534df092a215d59beb1bb0b042e15fe1b
mateja503 <matejanikolikj503@gmail.com>
2026-03-29 15:41:46 +0200

    created the te am compnent and the route in the sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,11 @@ import { Draft } from '../dashboard/draft/draft';
 import { Trade } from '../dashboard/trade/trade';
 import { League } from '../dashboard/league/league';
 import { Home } from '../dashboard/home/home';
-
+import { Team } from '../dashboard/team/team'; 
 export const routes: Routes = [
     { path: 'draft', component: Draft },
     { path: 'trade', component: Trade  },
     { path: 'league', component: League  },
     { path: 'home', component: Home },
+    { path: 'team', component: Team },
 ];
