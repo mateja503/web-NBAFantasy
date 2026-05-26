6263082d61ecf42d3583aca5703eb680c66e89dc
mateja503 <matejanikolikj503@gmail.com>
2026-05-26 18:11:56 +0200

    search players by name and remove svg

--- src/dashboard/draft/draft-list-players/draft-list-players.ts
+++ src/dashboard/draft/draft-list-players/draft-list-players.ts
@@ -1,17 +1,37 @@
-import { Component, Input } from '@angular/core';
+import { Component, computed, Input, signal } from '@angular/core';
 import { CommonModule } from '@angular/common'; // <-- 1. Import CommonModule
 import { DraftPlayer } from '../../../services/Hub/draftHub';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-draft-list-players',
-  standalone: true,                          // <-- Ensure standalone is true
-  imports: [CommonModule],                   // <-- 2. Add CommonModule here
+  standalone: true,                          
+  imports: [CommonModule,FormsModule],                  
   templateUrl: './draft-list-players.html',
   styleUrl: './draft-list-players.scss',
 })
 export class DraftListPlayers {
-  @Input() draftPlayers: DraftPlayer[] = []; // Now this will map safely from your parent template
-  
+
+  private playersSignal = signal<DraftPlayer[]>([]);
+
+  @Input() set draftPlayers(players: DraftPlayer[]) {
+    this.playersSignal.set(players);
+  }
+
+  public searchQuery = signal<string>('');
+
+  public filteredPlayers = computed(() => {
+    const query = this.searchQuery().toLowerCase();
+    if (!query) {
+      return this.playersSignal();
+    }
+
+    return this.playersSignal().filter(player =>
+      player.fullName.toLowerCase().includes(query)
+    );
+
+  })
+
   draftPlayer(playerId: number) {
     console.log(`Drafting player: ${playerId}`);
   }
